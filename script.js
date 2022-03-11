21ea48d770749c38340d146903c3135ab0ad04f8
Usama-Yasir <usamayasir32@gmail.com>
2022-03-12 04:07:18 +0500

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quizData = [
     {
-        question: 'How odl is Usama?',
+        question: 'How old is Usama?',
         a: '17',
         b: '10',
         c: '20',
@@ -34,10 +34,11 @@ const quizData = [
         c: '1996',
         d: 'none of the above',
         correct: 'd'
-    }
-    
+    }  
 ];
 
+const quiz = document.getElementById("quiz")
+const answersEls = document.querySelectorAll(".answer")
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -46,18 +47,55 @@ const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
-    const currentQuizData = quizData[currentQuestion];
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
-
-    currentQuestion++;
 }
 
-submitBtn.addEventListener('click, ()')
+function getSelected() {
+    let answer = undefined;
+
+    answersEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answersEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', () => {
+    //Check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct ) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML =  `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2> 
+            <button onclick = "location.reload()">Reload</button>`
+        }
+    }
+});
